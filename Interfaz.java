ceba87e631e8e9311f9ca46cc728115af219f868
alemanuel18 <jer24678@uvg.edu.gt>
2025-01-17 08:03:31 -0600

    Se a creado la interfaz y la clase encargada del funcionamiento de la licuadora

---
+++ Interfaz.java
@@ -0,0 +1,42 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Autor: Alejandro Manuel Jerez Melgar
+ * Fecha: 17 de enero de 2025
+ */
+
+public interface Interfaz {
+
+    /**
+     * Método para encender el sistema.
+     */
+    void encender();
+
+    /**
+     * Método para llenar el sistema.
+     */
+    void llenar();
+
+    /**
+     * Método para aumentar la velocidad.
+     */
+    void aumentarVelocidad();
+
+    /**
+     * Método para consultar la velocidad actual.
+     * 
+     * @return velocidad actual como un entero.
+     */
+    int consultarVelocidad();
+
+    /**
+     * Método para consultar si el sistema está lleno.
+     * 
+     * @return true si está lleno, false de lo contrario.
+     */
+    boolean consultarLlenado();
+
+    /**
+     * Método para vaciar el sistema.
+     */
+    void vaciar();
+}
---
+++ Licuadora.java
@@ -0,0 +1,72 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Autor: Alejandro Manuel Jerez Melgar
+ * Fecha: 17 de enero de 2025
+ */
+public class Licuadora implements Interfaz{
+
+    private boolean estadoLicuadora; /*true=On, false=Off */
+    private boolean capacidadLicuadora; /*true=llena, fasle==vacio */
+    private int velocidadLicuadora; /*empieza en cero */
+
+    /**
+     * 
+     * @param estadoLicuadora
+     * @param capacidadLicuadora
+     * @param velocidadLicuadora
+     */
+    public Licuadora(boolean estadoLicuadora, boolean capacidadLicuadora, int velocidadLicuadora){
+        this.estadoLicuadora=estadoLicuadora;
+        this.capacidadLicuadora=capacidadLicuadora;
+        this.velocidadLicuadora=velocidadLicuadora;
+    }
+
+    @Override
+    public void encender(){
+        if (capacidadLicuadora==true && estadoLicuadora==false) {
+            estadoLicuadora=true;
+            velocidadLicuadora=1;
+        }
+    }
+
+    @Override
+    public void llenar(){
+        if (capacidadLicuadora==false) {
+            capacidadLicuadora=true;
+        }
+    }
+
+    @Override
+    public void aumentarVelocidad(){
+        if (velocidadLicuadora<=10) {
+            velocidadLicuadora =+1;
+        }else{
+            velocidadLicuadora=0;
+        }
+    }
+
+    @Override
+    public int consultarVelocidad(){
+        return velocidadLicuadora;
+    }
+
+    @Override
+    public boolean consultarLlenado(){
+        return capacidadLicuadora;
+    }
+
+    @Override
+    public void vaciar(){
+        if (capacidadLicuadora==true) {
+            capacidadLicuadora=false;
+        }
+    }
+    /**
+     * 
+     * @return
+     */
+    public int getVelocidadLicuadora() {
+        return velocidadLicuadora;
+    }
+    
+}
